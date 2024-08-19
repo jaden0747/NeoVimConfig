e2028a3891001995f42f4da358321ff2efcee407
Ha Thanh Phong <thanhphong.ha@student.uts.edu.au>
2024-08-19 11:09:37 +0700

    initial commit

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,43 @@
+local lspconfig = require('lspconfig')
+
+-- Define your on_attach function
+local custom_on_attach = function(client, bufnr)
+	local caps = client.server_capabilities
+	if caps.semanticTokensProvider and caps.semanticTokensProvider.full then
+		local augroup = vim.api.nvim_create_augroup("SemanticTokens", {})
+		vim.api.nvim_create_autocmd("TextChanged", {
+			group = augroup,
+			buffer = bufnr,
+			callback = function()
+				vim.lsp.buf.semantic_tokens_full()
+			end,
+		})
+		-- fire it first time on load as well
+		vim.lsp.buf.semantic_tokens_full()
+	end	
+	require'completion'.on_attach(client)
+	require'diagnostic'.on_attach(client)
+	map('n','gD','<cmd>lua vim.lsp.buf.declaration()<CR>')
+	map('n','gd','<cmd>lua vim.lsp.buf.definition()<CR>')
+	map('n','K','<cmd>lua vim.lsp.buf.hover()<CR>')
+	map('n','gr','<cmd>lua vim.lsp.buf.references()<CR>')
+	map('n','gs','<cmd>lua vim.lsp.buf.signature_help()<CR>')
+	map('n','gi','<cmd>lua vim.lsp.buf.implementation()<CR>')
+	map('n','gt','<cmd>lua vim.lsp.buf.type_definition()<CR>')
+	map('n','<leader>gw','<cmd>lua vim.lsp.buf.document_symbol()<CR>')
+	map('n','<leader>gW','<cmd>lua vim.lsp.buf.workspace_symbol()<CR>')
+	map('n','<leader>ah','<cmd>lua vim.lsp.buf.hover()<CR>')
+	map('n','<leader>af','<cmd>lua vim.lsp.buf.code_action()<CR>')
+	map('n','<leader>ee','<cmd>lua vim.lsp.util.show_line_diagnostics()<CR>')
+	map('n','<leader>ar','<cmd>lua vim.lsp.buf.rename()<CR>')
+	map('n','<leader>=', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+	map('n','<leader>ai','<cmd>lua vim.lsp.buf.incoming_calls()<CR>')
+	map('n','<leader>ao','<cmd>lua vim.lsp.buf.outgoing_calls()<CR>')
+	
+end
+
+-- Setup clangd with custom on_attach function
+lspconfig.clangd.setup {
+    on_attach = custom_on_attach,
+    cmd = { "clangd", "--compile-commands-dir=./build" },
+}
---
+++ lua/jaden0747/init.lua
@@ -0,0 +1,8 @@
+require("jaden0747.packer")
+require("jaden0747.remap")
+require("jaden0747.treesitter")
+
+local o = vim.opt
+vim.cmd([[let g:netrw_bufsettings = 'noma nomod nu nobl nowrap ro']])
+vim.cmd([[set relativenumber]])
+
