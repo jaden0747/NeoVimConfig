08eb7a168d0dd3c0263fdf1931144ff0843cec26
Ha Thanh Phong <thanhphong.ha@student.uts.edu.au>
2024-08-19 11:48:40 +0700

    update lsp keymap

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -15,24 +15,8 @@ local custom_on_attach = function(client, bufnr)
 		-- fire it first time on load as well
 		vim.lsp.buf.semantic_tokens_full()
 	end	
-	require'completion'.on_attach(client)
-	require'diagnostic'.on_attach(client)
-	map('n','gD','<cmd>lua vim.lsp.buf.declaration()<CR>')
-	map('n','gd','<cmd>lua vim.lsp.buf.definition()<CR>')
-	map('n','K','<cmd>lua vim.lsp.buf.hover()<CR>')
-	map('n','gr','<cmd>lua vim.lsp.buf.references()<CR>')
-	map('n','gs','<cmd>lua vim.lsp.buf.signature_help()<CR>')
-	map('n','gi','<cmd>lua vim.lsp.buf.implementation()<CR>')
-	map('n','gt','<cmd>lua vim.lsp.buf.type_definition()<CR>')
-	map('n','<leader>gw','<cmd>lua vim.lsp.buf.document_symbol()<CR>')
-	map('n','<leader>gW','<cmd>lua vim.lsp.buf.workspace_symbol()<CR>')
-	map('n','<leader>ah','<cmd>lua vim.lsp.buf.hover()<CR>')
-	map('n','<leader>af','<cmd>lua vim.lsp.buf.code_action()<CR>')
-	map('n','<leader>ee','<cmd>lua vim.lsp.util.show_line_diagnostics()<CR>')
-	map('n','<leader>ar','<cmd>lua vim.lsp.buf.rename()<CR>')
-	map('n','<leader>=', '<cmd>lua vim.lsp.buf.formatting()<CR>')
-	map('n','<leader>ai','<cmd>lua vim.lsp.buf.incoming_calls()<CR>')
-	map('n','<leader>ao','<cmd>lua vim.lsp.buf.outgoing_calls()<CR>')
+	-- require'completion'.on_attach(client)
+	-- require'diagnostic'.on_attach(client)
 	
 end
 
@@ -41,3 +25,20 @@ lspconfig.clangd.setup {
     on_attach = custom_on_attach,
     cmd = { "clangd", "--compile-commands-dir=./build" },
 }
+
+vim.keymap.set('n','gD','<cmd>lua vim.lsp.buf.declaration()<CR>')
+vim.keymap.set('n','gd','<cmd>lua vim.lsp.buf.definition()<CR>')
+vim.keymap.set('n','K','<cmd>lua vim.lsp.buf.hover()<CR>')
+vim.keymap.set('n','gr','<cmd>lua vim.lsp.buf.references()<CR>')
+vim.keymap.set('n','gs','<cmd>lua vim.lsp.buf.signature_help()<CR>')
+vim.keymap.set('n','gi','<cmd>lua vim.lsp.buf.implementation()<CR>')
+vim.keymap.set('n','gt','<cmd>lua vim.lsp.buf.type_definition()<CR>')
+vim.keymap.set('n','<leader>gw','<cmd>lua vim.lsp.buf.document_symbol()<CR>')
+vim.keymap.set('n','<leader>gW','<cmd>lua vim.lsp.buf.workspace_symbol()<CR>')
+vim.keymap.set('n','<leader>ah','<cmd>lua vim.lsp.buf.hover()<CR>')
+vim.keymap.set('n','<leader>af','<cmd>lua vim.lsp.buf.code_action()<CR>')
+vim.keymap.set('n','<leader>ee','<cmd>lua vim.lsp.util.show_line_diagnostics()<CR>')
+vim.keymap.set('n','<leader>ar','<cmd>lua vim.lsp.buf.rename()<CR>')
+vim.keymap.set('n','<leader>=', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+vim.keymap.set('n','<leader>ai','<cmd>lua vim.lsp.buf.incoming_calls()<CR>')
+vim.keymap.set('n','<leader>ao','<cmd>lua vim.lsp.buf.outgoing_calls()<CR>')
